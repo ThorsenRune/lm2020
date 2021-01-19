38d813f52586a330b8546156876908e0a0cfae5d
ThorsenRune <thorsenrune@gmail.com>
2021-01-19 13:48:56 +0100

    sigh

---
+++ draft_esp/credentials.h
@@ -0,0 +1,6 @@
+// REPLACE WITH YOUR NETWORK CREDENTIALS
+const char* AP_SSID = "networkname";  // your router's SSID here
+const char* AP_PASS = "password";     // your router's password here
+const char* LM_URL = "http://thorsen.it/public/lm2020/lm_webapp";
+const char* SoftAP_SSID = "Arduino_LM";  //Name of the SoftAP - Arduino gets nicely first in the network list
+//Vedi ref201221 in google docs ...
