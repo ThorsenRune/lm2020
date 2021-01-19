079d7aa5fb2658380b4e9be96eaecff222f16170
ThorsenRune <thorsenrune@gmail.com>
2021-01-19 15:29:27 +0100

    Compiling OK. Lets test the WebSocket

--- draft_esp/draft_esp.ino
+++ draft_esp/draft_esp.ino
@@ -17,10 +17,22 @@ extern "C" {  //Note- neccessary to implement C files
   #include "system.h"
   #include "inoProtocol.h"      //Including h file allows you to access the functions
 }
-//#include "credentials.h"        //Default WIFI CREDENTIALS IN THIS FILE
+
 #include "getWiFiCreds.h" //establish connection to the  wifi accesspoint (WAP or internet WiFi router)
+
+//Define the pins for  U2UXD
+#define RXD2 16
+#define TXD2 17
+
+//Testing variable
+int nTimerInMs[3]={0};							// Milliseconds 1= T0 start of cycle 2=actual ms, 3How many clockcycles are available as resource
+
+bool bRelayLM2018 = false;    //Apply protocol to arduino FW or relay to LM_FW
+#if  ( DEBUG_ON!=1)
+
 //webSocket variables
 AsyncWebSocket ws("/ws");
+AsyncWebServer server2(80);
 AsyncWebSocketClient * globalClient = NULL;
 
 bool mStartWebSocket(IPAddress MyStaticIP,String AP_SSID,String AP_PASS){//This is the LM communication protocol
@@ -34,7 +46,6 @@ bool mStartWebSocket(IPAddress MyStaticIP,String AP_SSID,String AP_PASS){//This 
     }
     Serial.println(WiFi.localIP());
     //todo: copy code from lm_esp.ino line 38
-    AsyncWebServer server(80);
     IPAddress staticIP(MyStaticIP[0],MyStaticIP[1],MyStaticIP[2],MyStaticIP[3]); // parameters below are for your static IP address, if used
 
 
@@ -50,7 +61,7 @@ bool mStartWebSocket(IPAddress MyStaticIP,String AP_SSID,String AP_PASS){//This 
     return false;
   }
 
-
+/*
 void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
   //rt210114 Moved out of function.
     //AwsEventType describes what event has happened, like receive data or disconnetc
@@ -66,8 +77,21 @@ void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventT
     // mReceive(data,len);
   }
 }
-
-
+*/
+void onWsEvent(AsyncWebSocket * server2, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
+    //AwsEventType describes what event has happened, like receive data or disconnetc
+  // data is the payload
+  if(type == WS_EVT_CONNECT){
+    Serial.println("Websocket client connection received");
+    globalClient = client;
+  } else if(type == WS_EVT_DISCONNECT){
+    Serial.println("Websocket client connection finished");
+    globalClient = NULL;
+  } else if(type == WS_EVT_DATA){  //Data was received from client
+     mReceive(data,len);
+  }
+}
+#endif
 
 /*******   The two STANDARD ARDUINO functions (setup and loop)   **********/
 bool bWebSocketConnection =false;     //true when {mWIFIConnect == true}
@@ -76,25 +100,144 @@ void setup() {
  Serial.begin(115200);
  mDebugMsg("\nUnit testing\n");
  mDebugMsg("In Arduino remember to: Tools - Sketch upload ");
+#if  ( DEBUG_ON!=1)
  if(!SPIFFS.begin(true)){
   Serial.println("An Error has occurred while mounting SPIFFS");
   return;
   }
+  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);  //Connection to LM subsystem
   bWebSocketConnection=false;
+    Serial.println("Connecting to WiFi..");
   while (!bWebSocketConnection){
     bWebSocketConnection=mWIFIConnect();
   };      //Blocking until connection is made
-} //Now we proceed to {loop}
+    Serial.println(WiFi.localIP());
+    ws.onEvent(onWsEvent);
+    #endif
+    #if  ( DEBUG_ON!=1)
+      server2.begin();   //Todo4: do we need this?
+    #endif
+    MainSetup();		//Setup the system, protocol & .. (rt210107)
+    mDebugMsg("calling main loop");
+  #if ( DEBUG_ON==1)
+    mTesting();
+  #endif
+  //- Serial.printf("Receive buffer reset. Free: %i ",mFIFO_Free(oRX));
+
+  }// This returns to an intrinsic call to loop()
+
+  /*******************ARDUINO MAIN WHILE LOOP *******************************/
+  //      AUTOMATICALLTY CALLED BT ARDUINO IDE
+  void loop(){		//The main loop of the
+      nTimerInMs[2]++;    //Increase a timer just for fun
+      mSerialReceive();
+  	//delay(1);
 
 
-void loop() {
+  /*************      BUSINESS LOGIC FOR THE SIGNAL PROCESSING AND COMMUNICATION  *****/
+  //  TODO5 : enable / implement following calls
+  //+  mWaitCycleStart();							// 1.  Wait for a block start using system clocks
+  //+  mADCAux_Start();				//Todo1 maybe this corrupts EMG?
+  //+  if ( nMode.bits.SINEGENERATOR) mOutputSineWave();
+  //    mADCRestart(bFlip);
+  //    mOutputStimFSM_Debug(kReset);					//2. Reset statemachine for stimulaion and signals timing
+  //  mSignalProcessing();					//3. Signal Processing
+  //  mSystemActions();						//4.System management
+      mTransmit();  //send data to client. Corresponding  mReceive is interrupt handled
+      #if ( DEBUG_ON==1)
+        delay(1000);  //todo9: remove
+        mTesting2();
+      #endif
+  }
+  /******************************* END MAIN WHILE(1) *******************************/
 
-  if (!bWebSocketConnection) return;  //Only loop if on internetwifi
 
 
-}
 
-/* ENDOF ******   The two STANDARD ARDUINO functions**********/
+  int nCounter=0;
+  String sRcvdData;
+  long rssi=0;	//signal strength
+
+
+  /*************INTERFACE FOR SEND AND RECEIVE FROM CLIENT*************************/
+  // Get and Send data to client (the browser)
+
+  void mTransmit(){   //Transmit internal protocol data to client
+    //todo0:   oTX&RX are initialized in setup-->---->	MainSetup --->mCommInitialize
+    mCommunication();						//Process RX/TX buffers
+    if (!mFIFO_isEmpty(oTX)) mDebugMsg("mTransmit to client");
+    while (!mFIFO_isEmpty(oTX)){
+      uint8_t sendbyte=mPopTXFIFO();    //Get byte from  protocol
+      mSendToClient(sendbyte);
+      mDebugInt("sending",sendbyte);
+    }
+  }
+  void mReceive(uint8_t *data, size_t len){ //Get data from client
+  	sRcvdData="";
+  	int i=0;
+    mDebugInt("mReceive",len);
+      for( i=0; i < len; i++) {
+          mDebugInt("data",data[i]);
+          if (bRelayLM2018){  //Pass through to subdevice (LM)
+            sRcvdData=sRcvdData+(char) data[i];	//Fill up a buffer from RX0
+            Serial2.write(data[i]);		//Send incoming data  to LM
+          } else {  //push the data onto buffer
+            mPushRX2FIFO((char) data[i]);
+        }
+      }
+  	//if (i>0)Serial.printf("  Received data- pack length : %s \n" ,i);
+  //	Serial.print("RSSI:");	Serial.println(rssi);
+      //Serial.println(sRcvdData);		//onWsEvent read end
+  }
+  /*END ************INTERFACE FOR SEND AND RECEIVE FROM CLIENT*************************/
+  //	rssi = WiFi.RSSI();
+  #define MAXL 500
+  char mSendData[MAXL] ;	//'efg
+  int SendDataBuf=0;
+  void mSendToClient(uint8_t sendbyte ){
+    //Todo:merge 201222
+    //Put a byte on the queue to send to the client (wifi/bluetooth)
+    mSendData[SendDataBuf]=sendbyte;    //Get a byte from serial port
+    //Serial.print(SendDataBuf,DEC) ;Serial.print(": ");
+    //Serial.print(mSendData[SendDataBuf],DEC)  ;Serial.print(" \t ");
+    SendDataBuf++;
+  }
+  void mSerialReceive(){
+    #if ( DEBUG_ON==0)
+       if(globalClient != NULL && globalClient->status() == WS_CONNECTED){
+    	if (SendDataBuf>0){
+    		globalClient->binary(mSendData,SendDataBuf );
+    		Serial.printf("TX2 -> Client %d data\n ",SendDataBuf);
+    		SendDataBuf=0;
+    		delay(1); //Max messages per second =15 dont go below 100
+        	//note 201111
+    	  }
+       }
+    #endif
+  	while (Serial2.available()&&(SendDataBuf<MAXL)) {
+      int sendbyte=Serial2.read();
+      mSendToClient(sendbyte);
+  /* todo:delete		mSendData[SendDataBuf]=Serial2.read();    //Get a byte from serial port
+  		//Serial.print(SendDataBuf,DEC) ;Serial.print(": ");
+  		//Serial.print(mSendData[SendDataBuf],DEC)  ;Serial.print(" \t ");
+  		SendDataBuf++;*/
+  	}
+  //	if (SendDataBuf>0) Serial.printf("RX0 data: %d \n ",SendDataBuf);
+  }
+  void serialEvent2() {  //automatic event
+  	mSerialReceive();
+  }
+
+
+
+
+  /*
+  	note 201111
+  	Experimental findings:
+  	The serial port overflows at 256 received data . A 100 element vectorpack is 403 bytes
+  	No overflow, no serial data interrupt has been found, therefore the loop cant be
+  	delayed but the mSerialReceive must be polled frequently
+  */
 
 
 //  Debugging  message and continue
