a9a09eebc5a4c728d729b67c8585088ae720b859
Filippo Cinotti <cinottolo96@gmail.com>
2021-01-19 16:20:08 +0100

    Codice finale che funziona

--- draft_esp/credentials.h
+++ draft_esp/credentials.h
@@ -4,7 +4,7 @@
 
 String AP_SSID = "networkname";  // your router's SSID here
 String AP_PASS = "password";     // your router's password here
-String LM_URL = "http://thorsen.it/public/lm2020/lm_webapp";
+String LM_URL = "url";
 const char* SoftAP_SSID = "Arduino_LM";  //Name of the SoftAP - Arduino gets nicely first in the network list
 //Vedi ref201221 in google docs ...
 
